0228056576c76cebed0c8dc9432d7d79fea956c3
Alexandra <whitetomatoo@gmail.com>
2026-06-10 13:28:05 +1000

    Заполнение тестовыми постами

--- app/src/main/java/com/example/homework6/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/homework6/viewmodels/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.homework6.data.AppDatabase
 import com.example.homework6.data.entities.PostEntity
+import com.example.homework6.data.entities.PostTopicCrossRef
 import com.example.homework6.data.entities.TopicEntity
 import com.example.homework6.data.test_data.generateTestPosts
 import kotlinx.coroutines.Dispatchers
@@ -34,25 +35,47 @@ class MainViewModel(private val db: AppDatabase) : ViewModel() {
             Log.d("DEBUG_DB", "Текущее количество постов: $count")
 
             // Если постов нет (равно 0), то загружаем тестовые посты
-            if (count == 0) {
+            if (count == 13) {
                 Log.d("DEBUG_DB", "База пуста, генерируем посты...")
                 val testPosts = generateTestPosts() // List<TestPostData>
 
-                // Преобразуем List<TestPostData> -> List<PostEntity>
-                val entities = testPosts.map { testData ->
-                    PostEntity(
-                        id = 0,
-                        userId = 1,
-                        authorName = testData.post.authorName,
-                        title = testData.post.title,
-                        content = testData.post.content,
-                        imageUrl = testData.post.imageUrl
-                    )
-                }
+                testPosts.forEach { testData ->
+                    // 1. Вставляем сам пост
+                    val postToInsert = testData.post.copy(id = 0)
+                    val insertedPostId = db.postDao().insert(postToInsert).toLong()
 
-                db.postDao().insertAll(entities)
-                Log.d("DEBUG_DB", "Посты успешно вставлены: ${entities.size}")
+                    // 2. Формируем связи для таблицы post_topic
+                    val crossRefs = testData.topicIds.map { tId ->
+                        PostTopicCrossRef(
+                            postId = insertedPostId,
+                            topicId = tId
+                        )
+                    }
+
+                    // 3. Вставляем связи в базу
+                    if (crossRefs.isNotEmpty()) {
+                        db.postDao().insertPostTopics(crossRefs)
+                        Log.d("DEBUG_DB", "Для поста $insertedPostId добавлено тем: ${crossRefs.size}")
+                    }
+                }
+                Log.d("DEBUG_DB", "Генерация тестовых данных завершена")
             }
+
+//                // Преобразуем List<TestPostData> -> List<PostEntity>
+//                val entities = testPosts.map { testData ->
+//                    PostEntity(
+//                        id = 0,
+//                        userId = 1,
+//                        authorName = testData.post.authorName,
+//                        title = testData.post.title,
+//                        content = testData.post.content,
+//                        imageUrl = testData.post.imageUrl
+//                    )
+//                }
+//
+//                db.postDao().insertAll(entities)
+//                Log.d("DEBUG_DB", "Посты успешно вставлены: ${entities.size}")
+//            }
         }
     }
 }
